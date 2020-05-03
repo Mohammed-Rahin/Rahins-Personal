88e25c78336666549945aab9a1c8c5a9d5d25fd5
Mohammed Rahin <fj2921@wayne.edu>
2020-05-03 03:51:20 -0400

    GM project

--- app/src/main/java/com/example/gm/MainActivity.java
+++ app/src/main/java/com/example/gm/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
+    //On Create method
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
